f0e391307425a8b7a38b64b5ad5c5bae40e7f8b6
Bartosz Małż <bartek@bmalz.pro>
2016-07-11 23:29:47 +0200

    Connection to mssql database by tedious module

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Start!');
+var config = {
+    mssql: {
+        userName: "sa",
+        password: "",
+        server: "172.26.46.182",
+        database: "mdb"
+    }
+};
+
+var Connection = require("tedious").Connection;
+var connection = new Connection({
+    userName: config.mssql.userName,
+    password: config.mssql.password,
+    server: config.mssql.server,
+    options: {
+        database: config.mssql.database
+    }
+});
+
+connection.on("connect", function(error) {
+    if(error) {
+        return console.error(error);
+    } else {
+        console.log('Connected to ' + config.mssql.database + '!');
+    }
+});
