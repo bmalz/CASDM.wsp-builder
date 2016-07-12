b1802f7b0ef4e838b33a0f55e799cea69a797265
Bartosz Małż <bartek@bmalz.pro>
2016-07-12 21:37:52 +0200

    Fetch data from wsptbl and save it to list of objects

--- app.js
+++ app.js
@@ -35,21 +35,48 @@ eventEmitter.on('db_select_wsptbl', function(connection) {
     var Request = require('tedious').Request;
 
     var request = new Request("SELECT * FROM wsptbl", function(error) {
-        if(error) {
+        if (error) {
             console.error(error);
         }
     });
 
-    var result = '';
+    var wsptbl = new Array();
     request.on('row', function(columns) {
+        var wsptbl_item = new Object();
         columns.forEach(function(column) {
-            if(column.value !== null) {
-                result += column.metadata.colName + ": " + column.value + "\r\n";
+            if (column.value !== null) {
+                switch (column.metadata.colName) {
+                    case 'id':
+                        wsptbl_item.id = column.value;
+                        break;
+                    case 'table_name':
+                        wsptbl_item.table_name = column.value;
+                        break;
+                    case 'schema_name':
+                        wsptbl_item.schema_name = column.value;
+                        break;
+                    case 'description':
+                        wsptbl_item.description = column.value;
+                        break;
+                    case 'display_name':
+                        wsptbl_item.display_name = column.value;
+                        break;
+                    case 'function_group':
+                        wsptbl_item.function_group = column.value;
+                        break;
+                    case 'rel_attr':
+                        wsptbl_item.rel_attr = column.value;
+                        break;
+                    case 'common_name':
+                        wsptbl_item.common_name = column.value;
+                        break;
+                    default:
+                        break;
+                }
             }
         }, this);
 
-        console.log(result);
-        result = '';
+        wsptbl.push(wsptbl_item);
     });
     request.on('done', function(rowCount, more) {
         console.log(rowCount + ' rows returned');
